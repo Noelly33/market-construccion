4d13bd7864359fa52a46e5dabeea19430d0862c3
Tonny2712 <tonny.bermudezlli@ug.edu.ec>
2025-05-19 02:06:03 -0500

    cambios editar usuario ts

--- src/app/Components/pages/usuario/editar-usuario/editar-usuario.component.ts
+++ src/app/Components/pages/usuario/editar-usuario/editar-usuario.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-editar-usuario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './editar-usuario.component.html',
   styleUrl: './editar-usuario.component.css'
 })
-export class EditarUsuarioComponent {
 
+export class EditarUsuarioComponent {
+  
+  constructor(
+    private dialogRef: MatDialogRef<EditarUsuarioComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  guardar() {
+    this.data.activo = !!this.data.activo;
+    this.dialogRef.close(this.data);
+  }
+
+  cancelar() {
+    this.dialogRef.close();
+  }
 }
