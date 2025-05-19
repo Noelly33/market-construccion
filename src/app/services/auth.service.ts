ff1b2a7e3ac966720ed3ab287d1e0644831d06f2
Tonny2712 <tonny.bermudezlli@ug.edu.ec>
2025-05-19 02:14:32 -0500

    cambios auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
     currentUser$ = this.currentUserSubject.asObservable();
     currentRole$ = this.currentRoleSubject.asObservable();
 
-    login(username: string, password: string): boolean {
+    login(username: string, password: string):{ success: boolean, message: string, role?: string } {
       const user = this.validUsers.find(u => 
         u.username === username && u.password === password
       );
@@ -31,22 +31,32 @@ export class AuthService {
         this.isAuthenticatedSubject.next(true);
         this.currentUserSubject.next(user.name);
         this.currentRoleSubject.next(user.role);
-        return true;
+        localStorage.setItem('usuario_actual', JSON.stringify(user));
+        return { success: true, message: 'Acceso concedido (sistema)', role: user.role};
       }
-      return false;
-    }
 
-const clientes = JSON.parse(localStorage.getItem('clientes_registrados') || '[]');
+    
+
+
+
+  const clientes = JSON.parse(localStorage.getItem('clientes_registrados') || '[]');
   const cliente = clientes.find((c: any) => c.username === username && c.password === password);
 
   if (cliente) {
+    
+
+    if(!cliente.activo) {
+      
+      return { success: false, message: 'Tu cuenta está inactiva. No puedes iniciar sesión.' };
+    }
+
     this.isAuthenticatedSubject.next(true);
     this.currentUserSubject.next(cliente.nombre);
     this.currentRoleSubject.next(cliente.role); // 'user'
-    return true;
+    return { success: true, message: 'Acceso concedido (cliente)', role: cliente.role };
   }
-
-    return false;
+  
+    return { success: false, message: 'Usuario o contraseña incorrectos' };
   }
 
     logout(): void {
@@ -66,4 +76,6 @@ const clientes = JSON.parse(localStorage.getItem('clientes_registrados') || '[]'
     isLoggedIn(): boolean {
       return this.isAuthenticatedSubject.value;
     }
+
+    
 }
